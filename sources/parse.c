b402d84df0dfd66d104958bad408335d1e501442
Oscar <olaurine@student.42.fr>
2020-12-28 16:58:50 +0300

    args_increase

--- sources/parse.c
+++ sources/parse.c
@@ -6,7 +6,7 @@
 /*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 15:12:03 by olaurine          #+#    #+#             */
-/*   Updated: 2020/12/28 16:44:21 by olaurine         ###   ########.fr       */
+/*   Updated: 2020/12/28 16:56:00 by olaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,26 @@ void	parse_line(t_all *all, char *buf, int pos)
 	}
 }
 
-void	args_increase(t_all *all)
+int		args_increase(t_all *all)
 {
-	char **tmp;
-	if ((tmp = malloc(sizeof(char*) * (all->arg_len + 2))))
+	char	**tmp;
+	int		i;
+
+	if (!(tmp = malloc(sizeof(char*) * (all->arg_len + 2))))
+		return (1);
+	i = all->arg_len;
+	all->arg_len += 1;
+	tmp[i + 1] = 0;
+	tmp[i] = 0;
+	while (i > 0)
+	{
+		i--;
+		tmp[i] = all->args[i];
+	}
+	if (all->args)
+		free(all->args);
+	all->args = tmp;
+	return (0);
 }
 
 int		parse(t_all *all)
