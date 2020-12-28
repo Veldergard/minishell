9420270a24d46c19c6eb2e0873b25dfee2a5e9e7
Oscar <olaurine@student.42.fr>
2020-12-28 16:47:41 +0300

    push

--- includes/types.h
+++ includes/types.h
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   struct.h                                           :+:      :+:    :+:   */
+/*   types.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 20:25:22 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/27 20:25:22 by itressa          ###   ########.fr       */
+/*   Updated: 2020/12/28 16:18:59 by olaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
 
-#include "libft.h"
+# include "libft.h"
 
-typedef struct s_all {
-    char **args;
-    char **envp;
-    t_redirect *redirect; // for t_redirect;
-    int pipe; // 0, 1
-}              t_all;
+typedef struct			s_all {
+	char				**args;
+	int					arg_len;
+	char				**envp;
+	t_redirect			*redirect; // for t_redirect;
+	int					pipe; // 0, 1
+}						t_all;
 
-typedef struct s_redirect {
-    int               type; // 0 , 1 , 2
-    char              *filename;
-    struct s_redirect *next;
-}              t_redirect;
+typedef struct			s_redirect {
+    int					type; // 0 , 1 , 2
+    char				*filename;
+    struct s_redirect	*next;
+}						t_redirect;
 
 #endif
--- sources/parse.c
+++ sources/parse.c
@@ -6,7 +6,7 @@
 /*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 15:12:03 by olaurine          #+#    #+#             */
-/*   Updated: 2020/12/28 16:11:10 by olaurine         ###   ########.fr       */
+/*   Updated: 2020/12/28 16:44:21 by olaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 #include "parse.h"
 #include "get_next_line.h"
 
-void	parse_single_quote(t_all *all)
+void	parse_single_quote(t_all *all, char *buf, int *pos)
 {
+	while (buf[*pos] && buf[*pos] != '\'')
+	{
 
+	}
 }
 
 void	parse_double_quote(t_all *all)
@@ -40,13 +43,28 @@ void	parse_line(t_all *all, char *buf, int pos)
 	}
 }
 
-void	parse(t_all *all)
+void	args_increase(t_all *all)
+{
+	char **tmp;
+	if ((tmp = malloc(sizeof(char*) * (all->arg_len + 2))))
+}
+
+int		parse(t_all *all)
 {
 	char	*buf;
 
 	get_next_line(0, &buf);
 	if (!buf)
-		exit();
+		return (1);
+	if (!buf[0])
+	{
+		all->args = malloc(sizeof(char*) * 2);
+		all->args[0] = "exit";
+		all->args[1] = 0;
+		execute_cmd(all);
+		return (2);
+	}
 	parse_line(all, buf, 0);
 	free(buf);
+	return (0);
 }
