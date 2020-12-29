b0a70c1da7387b1820e92e48527fa60833d1e339
Ileana Tressa <itressa@ev-q2.kzn.21-school.ru>
2020-12-29 19:56:28 +0300

    fix girignore

--- sources/print_prompt.c
+++ sources/print_prompt.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   read_command.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
+/*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 17:38:13 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/28 18:17:09 by olaurine         ###   ########.fr       */
+/*   Updated: 2020/12/29 16:41:09 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "get_next_line.h"
 #include "color.h"
 
 void	print_prompt()
@@ -21,27 +20,6 @@ void	print_prompt()
 	pwd = getcwd(NULL, 0);
 	ft_putstr_fd(CLRLBLUE, 1);
 	ft_putstr_fd(pwd, 1);
-	ft_putstr_fd(CLRGREEN " |:ᐅ " CLRRESET, 1);
+	ft_putstr_fd(CLRGREEN " ᐅ " CLRRESET, 1);
 	free(pwd);
-}
-
-int		read_command()
-{
-	char	*buf;
-	pid_t	pid;
-	int		stat;
-
-	if (!(pid = fork()))
-	{
-		print_prompt();
-		get_next_line(0, &buf);
-		stat = ft_strncmp(buf, "exit", 4);
-		free(buf);
-		exit(stat);
-	}
-	else
-	{
-		waitpid(pid, &stat, 0);
-		return (stat);
-	}
 }
