77db24b4f60b2508dbe0bba481d35218fccc2870
itressa <itressa@student.21-school.ru>
2020-12-31 19:53:58 +0300

    exec now works with path env var

--- sources/exec/ft_exec.c
+++ sources/exec/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:16:14 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/30 17:34:27 by itressa          ###   ########.fr       */
+/*   Updated: 2020/12/31 19:44:39 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 #include "ft_error.h"
 #include <errno.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 char	*get_exec_cmd(t_all *all)
 {
+	int			i;
+	char		*cmd;
+	struct stat	buf;
+	char		*tmp;
+
 	if (ft_strchr(all->args[0], '/'))
 		return (ft_strdup(all->args[0]));
-	return (ft_strjoin(""/*TODO path*/, all->args[0]));
+	i = 0;
+	while (all->path[i])
+	{
+		tmp = ft_strjoin(all->path[i], "/");
+		cmd = ft_strjoin(tmp, all->args[0]);
+		free(tmp);
+		if (!stat(cmd, &buf))
+			return (cmd);
+		free(cmd);
+		i++;
+	}
+	return (ft_strdup(all->args[0]));
 }
 
 void	ft_exec(t_all *all)
@@ -31,7 +48,7 @@ void	ft_exec(t_all *all)
 	cmd = get_exec_cmd(all);
 	if (!(pid = fork()))
 	{
-		stat = execve(all->args[0], all->args, all->envp);
+		stat = execve(cmd, all->args, all->envp);
 		print_exec_error_errno(all->args[0]);
 		if (stat == -1)
 			exit(errno);
--- sources/types/init_t_all.c
+++ sources/types/init_t_all.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:20:47 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/30 17:21:23 by itressa          ###   ########.fr       */
+/*   Updated: 2020/12/31 16:58:30 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	init_t_all(t_all *all, char **envp)
 	all->envp = envp;
 	all->stdfd[0] = dup(0);
 	all->stdfd[1] = dup(1);
-	//all->path = ft_split(get_env(all, "PATH"), ':');
+	all->path = ft_split(get_env(all, "PATH", 4), ':');
 }
 
 void	destroy_t_all(t_all *all)
