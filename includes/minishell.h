b071b93c1bb763cc63a2ac40aeb175417a0993af
itressa <itressa@student.21-school.ru>
2021-01-02 20:30:49 +0300

    added function to print waitpid status information

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 16:11:05 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/31 15:58:06 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/02 17:16:45 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include "types.h"
 
-void	debug_print_arg(int argc, char *argv[], char *environ[]);
+void	debug_print_arg(int argc, char *argv[], char *envp[]);
 void	print_prompt();
 
 void	add_redirect(t_all *all, char *filename, int type);
--- sources/debug.c
+++ sources/debug.c
@@ -6,13 +6,13 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 17:34:24 by itressa           #+#    #+#             */
-/*   Updated: 2020/11/06 17:34:32 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/02 19:29:26 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	debug_print_arg(int argc, char *argv[], char *environ[])
+void	debug_print_arg(int argc, char *argv[], char *envp[])
 {
 	int		i;
 
@@ -31,11 +31,38 @@ void	debug_print_arg(int argc, char *argv[], char *environ[])
 	}
 	ft_putendl_fd("Environmental variables:", 1);
 	i = 0;
-	while (environ[i])
+	while (envp[i])
 	{
 		ft_putnbr_fd(i, 1);
 		ft_putstr_fd(": ", 1);
-		ft_putendl_fd(environ[i], 1);
+		ft_putendl_fd(envp[i], 1);
 		i++;
+	}
+}
+
+void	debug_wait_status(int stat)
+{
+	if (WIFEXITED(stat))
+	{
+		ft_putendl_fd("process exited", 2);
+		ft_putstr_fd("exit status: ", 2);
+		ft_putnbr_fd(WEXITSTATUS(stat), 2);
+		ft_putendl_fd("", 2);
+	}
+	if (WIFSIGNALED(stat))
+	{
+		ft_putendl_fd("process signaled", 2);
+		ft_putstr_fd("termination signal: ", 2);
+		ft_putnbr_fd(WTERMSIG(stat), 2);
+		ft_putendl_fd("", 2);
+		if (WCOREDUMP(stat))
+			ft_putstr_fd("core dumped", 2);
+	}
+	if (WIFSTOPPED(stat))
+	{
+		ft_putendl_fd("pricess stopped", 2);
+		ft_putstr_fd("stop signal: ", 2);
+		ft_putnbr_fd(WSTOPSIG(stat), 2);
+		ft_putendl_fd("", 2);
 	}
 }
