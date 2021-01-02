2739581a784ceba6f65213e405f1744c4bcdeafa
itressa <itressa@student.21-school.ru>
2021-01-02 15:21:00 +0300

    added builtin exit (dummy version)

--- sources/exec/ft_exec.c
+++ sources/exec/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:16:14 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/31 19:44:39 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/02 15:16:27 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,27 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
+int		ft_isbuiltin_cmd(char *cmd)
+{
+	int			i;
+	static char *builtins[8] =
+			{"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
+
+	i = 0;
+	while (builtins[i])
+	{
+		if (!ft_strncmp(builtins[i], cmd, 99))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int		ft_builtin(t_all *all)
+{
+	return (ft_exit(all->arg_len, all->args));
+}
 
 char	*get_exec_cmd(t_all *all)
 {
@@ -45,6 +66,8 @@ void	ft_exec(t_all *all)
 	char	*cmd;
 	int		stat;
 
+	if (ft_isbuiltin_cmd(all->args[0]))
+		ft_builtin(all);
 	cmd = get_exec_cmd(all);
 	if (!(pid = fork()))
 	{
