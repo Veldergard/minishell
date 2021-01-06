f0721659f9038cdc4516d51acb6fc141c81926c8
itressa <itressa@student.21-school.ru>
2021-01-06 16:35:26 +0300

    added cmd list functions

--- sources/types/cmd_list.c
+++ sources/types/cmd_list.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 15:51:42 by itressa           #+#    #+#             */
-/*   Updated: 2021/01/04 15:54:07 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/06 16:18:34 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,44 +18,37 @@ t_cmd			*ft_create_cmd(t_all *all)
 
 	if (!(cmd = malloc(sizeof(t_cmd))))
 		return (0);
-	cmd->args = 0;
-	cmd->arg_len = 0;
-	cmd->redirect = 0;
+	ft_bzero(cmd, sizeof(*cmd));
 	cmd->all_ptr = all;
-	cmd->pipe = PIPE_NO;
-	cmd->next = 0;
-	return (cmd);
-}
-
-void			ft_cmd_addfront(t_cmd **cmd, t_cmd *new)
-{
-	(void)cmd;
-	(void)new;
-}
-
-int				ft_cmdlen(t_cmd *cmd)
-{
-	(void)cmd;
-	return (0);
-}
-
-t_cmd			*ft_get_cmd_end(t_cmd *cmd)
-{
 	return (cmd);
 }
 
 void			ft_cmd_addback(t_cmd **cmd, t_cmd *new)
 {
-	(void)cmd;
-	(void)new;
-}
+	t_cmd	*current;
 
-void			ft_cmd_delete(t_cmd *cmd)
-{
-	(void)cmd;
+	if (!*cmd)
+	{
+		*cmd = new;
+		return ;
+	}
+	current = *cmd;
+	while (current->next)
+	{
+		current = current->next;
+	}
+	current->next = new;
 }
 
 void			ft_cmd_clearall(t_cmd **cmd)
 {
-	(void)cmd;
+	t_cmd	*next;
+
+	next = *cmd;
+	while (next)
+	{
+		*cmd = next->next;
+		free(next);
+		next = *cmd;
+	}
 }
