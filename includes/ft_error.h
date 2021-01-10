4c6fff598750ac93cecace16177b1e2b83f0d796
itressa <itressa@student.21-school.ru>
2021-01-10 17:13:22 +0300

    added dummy functions for errors

--- includes/ft_error.h
+++ includes/ft_error.h
@@ -6,12 +6,14 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 17:32:35 by itressa           #+#    #+#             */
-/*   Updated: 2020/12/30 17:33:03 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/10 15:30:28 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ERROR_H
 # define FT_ERROR_H
+
+# define FT_ERROR_NAME
 
 void	print_exec_error_errno(char *cmd);
 
--- sources/error/print_error.c
+++ sources/error/print_error.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 17:26:11 by itressa           #+#    #+#             */
-/*   Updated: 2021/01/02 14:59:28 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/10 15:17:52 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,14 @@ void	print_exec_error_errno(char *cmd)
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putendl_fd(error, 2);
+}
+
+void	print_exec_error_msg(char *cmd, char *message)
+{
+	ft_putendl_fd(message, 2);
+}
+
+void	print_exec_error_builtin(char *cmd, char *command, char *message)
+{
+	ft_putendl_fd(message, 2);
 }
