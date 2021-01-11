94f80ece0613c1ab73700813a4dbde6b9e0c41cc
itressa <itressa@student.21-school.ru>
2021-01-11 19:05:20 +0300

    fixed envlist sorting

--- sources/env/sort_envlist.c
+++ sources/env/sort_envlist.c
@@ -6,7 +6,7 @@
 /*   By: itressa <itressa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 14:42:36 by itressa           #+#    #+#             */
-/*   Updated: 2021/01/11 16:16:41 by itressa          ###   ########.fr       */
+/*   Updated: 2021/01/11 18:59:09 by itressa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,41 +23,29 @@ static int	compare_env_names(t_envlist *first, t_envlist *second)
 static void	sort_envlist_add(t_envlist **sorted, t_envlist *envlist)
 {
 	t_envlist	*current;
-	t_envlist	*tmp;
+	t_envlist	*insert;
 
-	current = *sorted;
-	if (!*sorted)
+	insert = ft_clone_envlist(envlist);
+	if (!*sorted || 0 < compare_env_names(*sorted, envlist))
 	{
-		*sorted = ft_clone_envlist(envlist);
-		return ;
-	}
-	else if (0 > compare_env_names(*sorted, envlist))
-	{
-		tmp = ft_clone_envlist(envlist);
-		tmp->next = *sorted;
-		*sorted = tmp;
-		return ;
+		insert->next = *sorted;
+		*sorted = insert;
 	}
 	else
-		while (current->next)
-		{
-			if (0 > compare_env_names(current, envlist))
-			{
-				tmp = ft_clone_envlist(envlist);
-				tmp->next = current->next->next;
-				current->next = tmp;
-				return ;
-			}
+	{
+		current = *sorted;
+		while (current->next && 0 > compare_env_names(current->next, envlist))
 			current = current->next;
-		}
-	current->next = ft_clone_envlist(envlist);
+		insert->next = current->next;
+		current->next = insert;
+	}
 }
 
 t_envlist	*ft_sort_envlist(t_envlist *envlist)
 {
 	t_envlist	*sorted;
 
-	sorted = NULL;
+	sorted = (t_envlist*)0;
 	while (envlist)
 	{
 		sort_envlist_add(&sorted, envlist);
