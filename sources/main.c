649cee489ff1ab3c37a4d9573954c3c3623acb98
Oscar <olaurine@student.42.fr>
2021-01-18 17:37:35 +0300

    feqw

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 16:03:44 by itressa           #+#    #+#             */
-/*   Updated: 2021/01/18 17:24:58 by olaurine         ###   ########.fr       */
+/*   Updated: 2021/01/18 17:25:05 by olaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,7 @@ void	parse_and_exec(t_all *all)
 		clear_all(all);
 		ret = parse(all);
 		if (all->pipe == PIPE_NO && last_piped == PIPE_YES && last_pipe > 0)
-		{
 			waitpid(last_pipe, &stat_loc, 0);
-		}
 		if (all->args)
 		{
 			ft_exec(all);
